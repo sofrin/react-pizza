8ad7b68c431b62d082d8dcd32eb5023724c1b71b
Sofrin <deadsalamanderninjatroll@gmail.com>
2022-09-19 08:53:01 +0700

    feat: add react-router

---
+++ src/components/NotFoundBlock/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import styles from './NotFoundBlock.module.scss';
+
+export const NotFoundBlock = () => {
+  return (
+    <div className={styles.root}>
+      <h1>
+        <span>:(</span>
+        <br />
+        Ничего не найдено
+      </h1>
+      <p className={styles.description}>
+        К сожалению данная страница отсуствует.
+      </p>
+    </div>
+  );
+};
---
+++ src/pages/Home.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import Categories from '../components/Categories';
+import PizzaBlock from '../components/PizzaBlock/PizzaBlock';
+import Skeleton from '../components/PizzaBlock/Skeleton';
+import Sort from '../components/Sort';
+
+export const Home = () => {
+  const [items, setItems] = React.useState([]);
+  const [isLoading, setisLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    fetch('https://62a75d89bedc4ca6d7c7c8ee.mockapi.io/items')
+      .then((res) => res.json())
+      .then((arr) => {
+        setItems(arr);
+        setisLoading(false);
+      });
+  }, []);
+
+  return (
+    <>
+      <div className='content__top'>
+        <Categories />
+        <Sort />
+      </div>
+      <h2 className='content__title'>Все пиццы</h2>
+      <div className='content__items'>
+        {isLoading
+          ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
+          : items.map((obj) => <PizzaBlock key={obj.id} {...obj} />)}
+      </div>
+    </>
+  );
+};
